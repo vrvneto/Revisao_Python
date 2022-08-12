2e512aa1af66e393875f0279e62fcfc175686af8
Valdenor Neto <vrvneto@hotmail.com>
2022-08-12 15:37:52 -0300

    Create tipo_booleano.py
    Tipo Booleano. atribuição de dois valores(True, False)

---
+++ tipo_booleano.py
@@ -0,0 +1,27 @@
+"""
+Tipo Booleano
+Algebra booleano criada por George Boole.
+2 constantes, Verdadeiro ou Falso.
+True -> Verdadeiro.
+False -> Falso
+OBS: sempre com inicial maiúscula.
+"""
+
+ativo = True
+print(ativo)
+
+"""
+Operações Básicas:
+"""
+
+#Negação (not):
+"""
+Fazendo a negação, se o valor booleano for verdadeiro o resultado será falso,
+e se for falso o resultado será verdadeiro. Ou seja, sempre o contrário.
+"""
+print(not ativo)
+
+"""
+Tipo Booleano. atribuição de dois valores(True, False).
+Utilizado em conjunto com as estruturas lógicas.
+"""
