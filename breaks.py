8f5d74bd30096eb39fb975362ccf6e3f39107a67
Valdenor Neto <vrvneto@hotmail.com>
2022-08-15 22:00:07 -0300

    Create breaks.py
    Saindo de loops com 'break'.

---
+++ breaks.py
@@ -0,0 +1,19 @@
+"""
+Saindo de loops com break.
+
+utilizamos o break para sair de loops de maneira projetada
+
+"""
+# Exemplo 1
+for numero in range(1, 11):
+    if numero == 6:
+        break
+    else:
+        print(numero)
+print('Sai do loop!')
+
+# Exemplo 2
+while True:
+    comando = input("Digite 'sair' para sair")
+    if comando == 'sair':
+        break
