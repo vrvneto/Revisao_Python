096224b5f307226f66e28fc010b255600c53156c
Valdenor Neto <vrvneto@hotmail.com>
2022-08-18 15:07:26 -0300

    Create listas.py
    Listas em Python

---
+++ listas.py
@@ -0,0 +1,7 @@
+"""
+LISTAS
+
+- Listas em python funciona como vetores/matrizes (arrays) em outras linguagens, com a
+  diferença de serem DINÂMICO e tambem de podermos colocar qualquer tipo de dado.
+
+"""
