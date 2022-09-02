97ec8f395c2da321b32ee13ea029554adc54923d
Valdenor Neto <vrvneto@hotmail.com>
2022-09-01 21:47:40 -0300

    Create tuplas.py

---
+++ tuplas.py
@@ -0,0 +1,17 @@
+"""
+- Tuplas (tuple)
+
+Tuplas são bem parecidas com listas, existem duas diferenças básicas:
+- As tuplas são representadas por parenteses ()
+- As tuplas são imutáveis, ao se criar uma tupla ela não muda. toda operação
+  em uma tupla gera uma nova tupla.
+
+"""
+# CUIDADO 1: as tuplas são representadas por parenteses, mas veja:
+tupla1 = (1, 2, 3, 4, 5, 6)
+print(tupla1)
+print(type(tupla1))
+
+tupla2 = 1, 2, 3, 4, 5, 6
+print(tupla2)
+print(type(tupla2))
