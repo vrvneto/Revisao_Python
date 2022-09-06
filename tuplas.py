23b41cb7a4f1b7992ffd5d54e2252fbdb040c28e
Valdenor Neto <vrvneto@hotmail.com>
2022-09-05 21:27:08 -0300

    Update tuplas.py

--- tuplas.py
+++ tuplas.py
@@ -15,3 +15,25 @@ print(type(tupla1))
 tupla2 = 1, 2, 3, 4, 5, 6
 print(tupla2)
 print(type(tupla2))
+
+# CUIDADO 2: Tuplas com 1 elemento
+
+tupla3 = (4)# Isso não é uma tupla
+print(tupla3)
+print(type(tupla3))
+
+tupla4 = (4,)
+print(tupla4) # Isso é uma tupla
+print(type(tupla4))
+
+tupla5 = 5,
+print(tupla5)
+print(type(tupla5))
+
+# CONCLUSÂO: Podemos concluir que tuplas são definidas pela virgula e não pelo parenteses
+
+# Tupla pode ser criada por um 'range'.
+tupla6 = tuple(range(11))
+print(tupla6)
+print(type(tupla6))
+
